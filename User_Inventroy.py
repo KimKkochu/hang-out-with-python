0dfd5a9a5c6eb3e73ccd849e00a5bccc831ec8c9
KimKkochu <kimkkochu69@gmail.com>
2023-01-02 17:29:21 +0900

    User Name duplicate checking & item push inventory

--- User_Inventroy.py
+++ User_Inventroy.py
@@ -1,24 +1,23 @@
+User_Name = set()
+
 class User:
     def __init__(self, Name:str) -> None:
         # 0 equal 'empty'
         self.default_setting = {
             'Name': Name,
-            'inventory' : [[{0:0} for i in range(3)] for i in range(3)]
+            'inventory' : [[{0:0} for i in range(3)] for i in range(3)],
+            'Stack_point': [0, 0] # [row, column]
         }
         self.r_hand_set(0, 0)
+        User_Name.add(Name)
         
     def r_hand_set(self, row:int, column:int):
         self.default_setting['R_hand'] = self.default_setting['inventory'][row][column]
-    
-    def get_item(self, item):
-        pass
-
 
 class Lighter(User):
-    def __init__(self, Name: str) -> None:
-        super().__init__(Name)
+    def __init__(self, Name):
+        pass
+
         
 
-Person = User('Elon')
-
-print(Person.default_setting)
+Person = User(Name='Elon')
