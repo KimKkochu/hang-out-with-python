5496cdafe89f637942260cb6658f5d43c2469b66
KimKkochu <kimkkochu69@gmail.com>
2023-01-03 16:14:33 +0900

    dup name checking

--- User_Inventroy.py
+++ User_Inventroy.py
@@ -1,15 +1,18 @@
-User_Name = set()
+User_Name = ['Elon']
 
 class User:
     def __init__(self, Name:str) -> None:
-        # 0 equal 'empty'
-        self.default_setting = {
-            'Name': Name,
-            'inventory' : [[{0:0} for i in range(3)] for i in range(3)],
-            'Stack_point': [0, 0] # [row, column]
-        }
-        self.r_hand_set(0, 0)
-        User_Name.add(Name)
+        if Name in User_Name:
+            print('dup name existence')
+            self.default_setting = {'Name': 'one more try', '...':'...'}
+        else:
+            # 0 equal 'empty'
+            self.default_setting = {
+                'Name': Name,
+                'inventory' : [[{0:0} for i in range(3)] for i in range(3)],
+                'Stack_point': [0, 0] # [row, column]
+            }
+            self.r_hand_set(0, 0)
         
     def r_hand_set(self, row:int, column:int):
         self.default_setting['R_hand'] = self.default_setting['inventory'][row][column]
@@ -18,6 +21,6 @@ class Lighter(User):
     def __init__(self, Name):
         pass
 
-        
+Person = User(Name='yeah')
+print(Person.default_setting)
 
-Person = User(Name='Elon')
